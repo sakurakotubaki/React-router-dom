8861c602650c2e71c54dece46580c4996b5b69a4
sakurakotubaki <73347485+sakurakotubaki@users.noreply.github.com>
2021-09-05 19:21:04 +0900

    テンプレート文字列に置き換え

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-
-import { Page1 } from "../Page1";
-import { Page2 } from "../Page2";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import { Home } from "./Home";
 import "./styles.css";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 export default function App() {
   return (
@@ -19,9 +20,25 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route exact path="/page1">
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
