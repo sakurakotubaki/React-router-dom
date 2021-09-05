b607d4524a243c9cd9d09ceb395e066cab819ed3
sakurakotubaki <73347485+sakurakotubaki@users.noreply.github.com>
2021-09-05 19:53:42 +0900

    ルートをコンポーネント化した Routerのコンポーネントを作成して、ページ遷移を可能にした

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Home } from "./Home";
+import { BrowserRouter, Link } from "react-router-dom";
+
+import { Router } from "./router/Router";
 import "./styles.css";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
 
 export default function App() {
   return (
@@ -16,33 +13,7 @@ export default function App() {
         <br />
         <Link to="/Page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route exact path="/page1">
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
